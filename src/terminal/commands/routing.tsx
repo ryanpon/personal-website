398d32b8f9bc1fefc44c59cc8097fdd9b51910f5
Ryan Pon <me@ryanpon.com>
2026-04-25 12:33:00 -0700

    Refactor utilities

--- src/terminal/commands/routing.tsx
+++ src/terminal/commands/routing.tsx
@@ -1,64 +1,7 @@
 import { useEffect, useState } from "react";
 import { colorSpan, colors } from "../colors";
+import { MinHeap } from "../minheap";
 import type { AppExit, Command } from "./types";
-
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  push(key, val) {
-    this.heap.push([key, val]);
-    this._bubbleUp(this.heap.length - 1);
-  }
-
-  pop() {
-    const min = this.heap[0];
-    const last = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = last;
-      this._sinkDown(0);
-    }
-    return min;
-  }
-
-  peek() {
-    return this.heap[0];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  _bubbleUp(i) {
-    while (i > 0) {
-      const parent = Math.floor((i - 1) / 2);
-      if (this.heap[parent][0] <= this.heap[i][0]) break;
-      [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
-      i = parent;
-    }
-  }
-
-  _sinkDown(i) {
-    const n = this.heap.length;
-    while (true) {
-      let smallest = i;
-      const l = 2 * i + 1, r = 2 * i + 2;
-      if (l < n && this.heap[l][0] < this.heap[smallest][0]) smallest = l;
-      if (r < n && this.heap[r][0] < this.heap[smallest][0]) smallest = r;
-      if (smallest === i) break;
-      [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
-      i = smallest;
-    }
-  }
-}
-
-function pad(s: string | number, width: number, right = false) {
-  const str = String(s);
-  if (str.length >= width) return str.slice(0, width);
-  const fill = " ".repeat(width - str.length);
-  return right ? str + fill : fill + str;
-}
 
 function dist(x1, y1, x2, y2) {
   return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
